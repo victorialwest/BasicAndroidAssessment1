b00dd216f55195a6fec7a3cf25fa5dc05781fb0b
Victoria West <victoriawest811@gmail.com>
2018-03-16 15:29:51 -0400

    creating repo

--- app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
@@ -9,10 +9,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView
+    private EditText
+    private Button
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView sampletextview = (TextView)  findViewById(R.id.sample_textview;
+        EditText sampleedittext = (EditText) findViewById(R.id.sample_edittext);
+        Button sampleButton = (Button) findViewById(R.id.sample_button);
+
+
+
+
     }
+
+  protected void onClickListener(){
+   sampleButton.setOnClickListener;
+
+    }
+
+
+
 }
